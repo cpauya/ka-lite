c70435f5c3b207e41064b5ceffb25200d61bc8a3
Eduard James Aban <eduard.aban@mrpau.com>
2017-04-24 16:12:41 +0800

    no message

--- .buildkite/build_whl.sh
+++ .buildkite/build_whl.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
+pip install -e .
 make dockerenvdist
+
 buildkite-agent artifact upload 'dist/*.whl'
 buildkite-agent artifact upload 'dist/*.zip'
 buildkite-agent artifact upload 'dist/*.tar.gz'
