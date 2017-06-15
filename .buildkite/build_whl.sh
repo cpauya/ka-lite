750fddffae392f3e996ea45ad62cc7654b8ab750
Eduard James Aban <eduard.aban@mrpau.com>
2017-06-15 16:33:22 +0800

    no message

--- .buildkite/build_whl.sh
+++ .buildkite/build_whl.sh
@@ -2,11 +2,7 @@
 
 set -euo pipefail
 
-pip install pex   
-pip install wheel
 make dockerenvdist
-make pex 
 buildkite-agent artifact upload 'dist/*.whl'
 buildkite-agent artifact upload 'dist/*.zip'
 buildkite-agent artifact upload 'dist/*.tar.gz'
-buildkite-agent artifact upload 'dist/*.pex'
