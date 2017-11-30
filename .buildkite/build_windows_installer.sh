0ab7ec46df16a8b45c6a3a21134bf8e5ebaaa6c6
Eugene Oliveros <eugene.oliveros@mrpau.com>
2017-11-30 15:17:47 +0800

    Now I know why

--- .buildkite/build_windows_installer.sh
+++ .buildkite/build_windows_installer.sh
@@ -8,11 +8,6 @@ KALITE_WINDOWS_PATH="$KALITE_DOCKER_PATH/ka-lite-installers/windows"
 STEP=1
 STEPS=5
 
-echo "Working Directory: $PARENT_PATH"
-ls -l $PARENT_PATH
-echo "Docker path?"
-ls -l "$PARENT_PATH/windows_installer_docker_build"
-
 
 # Download artifacts to dist/
 echo "$STEP of $STEPS"
@@ -24,12 +19,10 @@ cat "$PARENT_PATH/kalite/VERSION"
 echo "$STEP of $STEPS"
 # Clone KA-Lite windows installer and download content pack
 cd $KALITE_DOCKER_PATH
-git clone https://github.com/learningequality/ka-lite-installers.git && git checkout 0.17.x
+git clone https://github.com/learningequality/ka-lite-installers.git && cd ka-lite-installers && git checkout 0.17.x
 cd $KALITE_WINDOWS_PATH # && wget http://pantry.learningequality.org/downloads/ka-lite/0.17/content/contentpacks/en.zip
 ((STEP++))
 
-echo "Was the dockerfile deleted?"
-ls -l "$KALITE_DOCKER_PATH"
 echo "$STEP of $STEPS"
 # Copy kalite whl files to kalite windows installer path
 COPY_CMD="cp $PARENT_PATH/dist/*.whl $KALITE_WINDOWS_PATH"
@@ -40,8 +33,10 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 ((STEP++))
+
 echo "$STEP of $STEPS"
 KALITE_BUILD_VERSION=$(cat $PARENT_PATH/kalite/VERSION)
+
 # Build KA-Lite windows installer docker image
 cd $KALITE_DOCKER_PATH
 echo "Docker Path: $KALITE_DOCKER_PATH"
@@ -51,27 +46,24 @@ if [ ! -f "$PARENT_PATH/windows_installer_docker_build/Dockerfile" ]; then
     exit 1
 fi
 
-echo "$PWD"
-echo "Files inside this directory"
-ls -l .
-# DOCKER_BUILD_CMD="docker image build -t $KALITE_BUILD_VERSION-build ."
-# $DOCKER_BUILD_CMD
+DOCKER_BUILD_CMD="docker image build -t $KALITE_BUILD_VERSION-build ."
+$DOCKER_BUILD_CMD
 
-# if [ $? -ne 0 ]; then
-#     echo "... Abort! Error running $DOCKER_BUILD_CMD."
-#     exit 1
-# fi
+if [ $? -ne 0 ]; then
+    echo "... Abort! Error running $DOCKER_BUILD_CMD."
+    exit 1
+fi
 
-# INSTALLER_PATH="$KALITE_DOCKER_PATH/installer"
-# mkdir -p $INSTALLER_PATH
+INSTALLER_PATH="$KALITE_DOCKER_PATH/installer"
+mkdir -p $INSTALLER_PATH
 
-# # Run KA-Lite windows installer docker image.
-# DOCKER_RUN_CMD="docker run -v $INSTALLER_PATH:/installer/ $KALITE_BUILD_VERSION-build"
-# $DOCKER_RUN_CMD
+# Run KA-Lite windows installer docker image.
+DOCKER_RUN_CMD="docker run -v $INSTALLER_PATH:/installer/ $KALITE_BUILD_VERSION-build"
+$DOCKER_RUN_CMD
 
-# if [ $? -ne 0 ]; then
-#     echo "... Abort! Error running $DOCKER_RUN_CMD."
-#     exit 1
-# fi
+if [ $? -ne 0 ]; then
+    echo "... Abort! Error running $DOCKER_RUN_CMD."
+    exit 1
+fi
 
-# buildkite-agent artifact upload '/installer/*.exe'
+buildkite-agent artifact upload '/installer/*.exe'
